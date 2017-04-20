1444ba5b69dd603ba32dfded9f5ba86d45760d97
Fabian Seitz <seitzf1@yahoo.de>
2017-04-20 14:17:35 +0200

    Update

--- Windows Afterinstall/InstallDriver-Windows7.bat
+++ Windows Afterinstall/InstallDriver-Windows7.bat
@@ -1,8 +1,27 @@
 @echo off
 title Treiber Installation by Fabian Seitz
 color 89
+
+:Default
+cls
+start https://ninite.com/7zip-chrome/ninite.exe
+echo             ================================================
+echo                               Automatische
+echo                   Treiber Installation by Fabian Seitz
+echo                              Fuer Windows 10
+echo             ================================================
+echo.
+echo            Als Administrator gestartet?
+echo            Downloade Google Chrome ! Stark empfohlen!
+echo.
+echo Warte auf Beendigung des Downloads, dann ...
+pause
+goto :Start
+
 :Start
 cd C:\Users\%username%\Downloads\
+Ren "C:\Users\%username%\Downloads\Ninite 7Zip Chrome Installer.exe" Ninite-7zip-Chrome.exe
+start C:\Users\%username%\Downloads\Ninite-7zip-Chrome.exe
 cls
 echo             ================================================
 echo                               Automatische
@@ -19,7 +38,6 @@ echo   [2]    Latitude E6510 / Notebook
 echo   [3]    Precision M6500 / CAD Notebook
 echo   [4]    Precision T5500 / CAD PC
 echo.
-echo   [C]    Google Chrome
 echo   [U]    Windows 7 Update Pack
 echo   [0]    EXIT / Abbruch
 echo.
@@ -34,9 +52,7 @@ if %asw%==2 goto :E6510
 if %asw%==3 goto :M6500
 if %asw%==4 goto :T5500
 
-if %asw%==C goto :Chrome
 if %asw%==U goto :Update
-if %asw%==S goto :Settings
 if %asw%==0 goto :exit
 if %asw%==exit goto :exit
 
@@ -101,16 +117,6 @@ echo Dialog schließt sich in 5 Sekunden und loescht Installationsfiles.
 del /q C:\Users\%username%\Downloads\*.exe
 ping -n 6 127.0.0.1 > nul
 goto :exit
-
-
-
-:Chrome
-start https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B3A60215A-36F6-43BF-B717-7EC844B44C7F%7D%26lang%3Dde%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Ddefaultbrowser/update2/installers/ChromeSetup.exe
-ping -n 4 127.0.0.1 > nul
-start ChromeSetup.exe /silent /install
-echo Chrome wird im Hintergrund installiert
-pause
-goto :Start
 
 :Update
 start http://winfuture.de/downloadvorschalt,2671.html
