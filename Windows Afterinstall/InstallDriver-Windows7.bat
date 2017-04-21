f688917339304bd6e8675a8d15994877c7c0f853
Fabian Seitz <seitzf1@yahoo.de>
2017-04-21 11:42:37 +0200

    Update InstallDriver-Windows7.bat

--- Windows Afterinstall/InstallDriver-Windows7.bat
+++ Windows Afterinstall/InstallDriver-Windows7.bat
@@ -2,27 +2,9 @@
 title Treiber Installation by Fabian Seitz
 color 89
 
-:Default
-cls
-start https://ninite.com/7zip-chrome/ninite.exe
-echo             ================================================
-echo                               Automatische
-echo                   Treiber Installation by Fabian Seitz
-echo                              Fuer Windows 10
-echo             ================================================
-echo.
-echo            Als Administrator gestartet?
-echo            Downloade Google Chrome ! Stark empfohlen!
-echo.
-echo Warte auf Beendigung des Downloads, dann ...
-pause
-goto :Start
-
 :Start
-cd C:\Users\%username%\Downloads\
-Ren "C:\Users\%username%\Downloads\Ninite 7Zip Chrome Installer.exe" Ninite-7zip-Chrome.exe
-start C:\Users\%username%\Downloads\Ninite-7zip-Chrome.exe
 cls
+cd C:\Users\%username%\Downloads\
 echo             ================================================
 echo                               Automatische
 echo                   Treiber Installation by Fabian Seitz
@@ -60,9 +42,19 @@ echo Nächste Auswahl? Bitte eine Zahl von oben waehlen!
 goto:Auswahl
 
 :E6520
-start https://downloadcenter.intel.com/downloads/eula/25977/Intel-Graphics-Driver-for-Windows-10-and-Windows-7-8-1-15-33-?httpDown=https%3A%2F%2Fdownloadmirror.intel.com%2F25977%2Feng%2Fwin64_153343.4425.exe
-start https://downloadcenter.intel.com/downloads/eula/26656/Intel-PROSet-Wireless-Software-and-Drivers-for-Windows-7-?httpDown=https%3A%2F%2Fdownloadmirror.intel.com%2F26656%2Feng%2FWireless_19.50.0_PROSet64_Win7.exe
-start https://downloadcenter.intel.com/downloads/eula/18713/Intel-Network-Adapter-Driver-for-Windows-7-?httpDown=https%3A%2F%2Fdownloadmirror.intel.com%2F18713%2Feng%2FPROWinx64.exe
+start https://ninite.com/7zip-chrome/ninite.exe
+echo Warte auf Beendigung des Downloads, dann ...
+pause
+Ren "C:\Users\%username%\Downloads\Ninite 7Zip Chrome Installer.exe" Ninite-7zip-Chrome.exe
+start C:\Users\%username%\Downloads\Ninite-7zip-Chrome.exe
+echo Warte auf Beendigung der Chrome Installation, dann ...
+pause
+start ChromeDefaultBrowser.vbs
+echo Bei Fehlermeldung konnte nicht gefunden werden bitte Chrome manuell als Standard Browser einstellen! Einstellungen -> Apps -> Standard-Apps
+ping -n 3 127.0.0.1 > nul
+start https://downloadmirror.intel.com/25977/eng/win64_153343.4425.exe
+start https://downloadmirror.intel.com/26656/eng/Wireless_19.50.0_PROSet64_Win7.exe
+start https://downloadmirror.intel.com/18713/eng/PROWinx64.exe
 start https://downloads.dell.com/input/DRVR_WIN_R311554.EXE
 echo Warte auf Beendigung des Downloads, dann ...
 pause
@@ -76,8 +68,20 @@ ping -n 6 127.0.0.1 > nul
 goto :exit
 
 :E6510
-start https://downloadcenter.intel.com/de/downloads/eula/18713/Intel-Netzwerkadapter-Treiber-f-r-Windows-7-?httpDown=https%3A%2F%2Fdownloadmirror.intel.com%2F18713%2Feng%2FPROWinx64.exe
-start https://downloads.dell.com/network/INTEL_825XX-GIGABIT-PLATFORM_A06_R290983.exe
+start E:\E6510-WLAN-Driver.exe
+start E:\E6510-EThernet-Driver.exe
+echo Warte auf Beendigung der Installationen, dann ...
+pause
+start https://ninite.com/7zip-chrome/ninite.exe
+echo Warte auf Beendigung des Downloads, dann ...
+pause
+Ren "C:\Users\%username%\Downloads\Ninite 7Zip Chrome Installer.exe" Ninite-7zip-Chrome.exe
+start C:\Users\%username%\Downloads\Ninite-7zip-Chrome.exe
+echo Warte auf Beendigung der Chrome Installation, dann ...
+pause
+start ChromeDefaultBrowser.vbs
+echo Bei Fehlermeldung konnte nicht gefunden werden bitte Chrome manuell als Standard Browser einstellen! Einstellungen -> Apps -> Standard-Apps
+ping -n 3 127.0.0.1 > nul
 start https://downloads.dell.com/video/VIDEO_DRVR_WIN_R296701.EXE
 start https://downloads.dell.com/FOLDER01449251M/1/Input_Driver_GGY5W_WN_8.1200.101.127_A06.EXE
 echo Warte auf Beendigung des Downloads, dann ...
@@ -91,8 +95,17 @@ ping -n 6 127.0.0.1 > nul
 goto :exit
 
 :M6500
-start http://www.driverscape.com/files/DriverToolkitInstaller.exe
-start https://www2.ati.com/drivers/firepro/mobile/dell-mobile-radeon-pro-software-enterprise-17.q1.1-apr3.exe
+start https://ninite.com/7zip-chrome/ninite.exe
+echo Warte auf Beendigung des Downloads, dann ...
+pause
+Ren "C:\Users\%username%\Downloads\Ninite 7Zip Chrome Installer.exe" Ninite-7zip-Chrome.exe
+start C:\Users\%username%\Downloads\Ninite-7zip-Chrome.exe
+echo Warte auf Beendigung der Chrome Installation, dann ...
+pause
+start ChromeDefaultBrowser.vbs
+echo Bei Fehlermeldung konnte nicht gefunden werden bitte Chrome manuell als Standard Browser einstellen! Einstellungen -> Apps -> Standard-Apps
+ping -n 3 127.0.0.1 > nul
+start http://support.amd.com/en-us/download/workstation/mobile?oem=Dell&os=Windows+7+-+64
 start https://downloads.dell.com/input/PREM6500_DRVR_WIN_R280879.EXE
 start https://downloads.dell.com/FOLDER01694314M/2/Network_Driver_3MNPT_WN_15.10.0.10_A06.EXE
 echo Warte auf Beendigung des Downloads, dann ...
@@ -106,6 +119,16 @@ ping -n 6 127.0.0.1 > nul
 goto :exit
 
 :T5500
+start https://ninite.com/7zip-chrome/ninite.exe
+echo Warte auf Beendigung des Downloads, dann ...
+pause
+Ren "C:\Users\%username%\Downloads\Ninite 7Zip Chrome Installer.exe" Ninite-7zip-Chrome.exe
+start C:\Users\%username%\Downloads\Ninite-7zip-Chrome.exe
+echo Warte auf Beendigung der Chrome Installation, dann ...
+pause
+start ChromeDefaultBrowser.vbs
+echo Bei Fehlermeldung konnte nicht gefunden werden bitte Chrome manuell als Standard Browser einstellen! Einstellungen -> Apps -> Standard-Apps
+ping -n 3 127.0.0.1 > nul
 start http://www.nvidia.de/content/DriverDownload-March2009/confirmation.php?url=/Windows/Quadro_Certified/377.11/377.11-quadro-grid-desktop-notebook-win10-64bit-international-whql.exe&lang=de&type=Quadro
 start https://downloads.dell.com/FOLDER00308409M/1/Network_Driver_YM9NC_WN32_17.0.2_A00.EXE
 echo Warte auf Beendigung des Downloads, dann ...
