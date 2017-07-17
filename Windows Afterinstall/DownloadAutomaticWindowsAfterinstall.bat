a37d20aac62d1373cbc74fd3573e9a2a51c87433
Fabian Seitz <seitzf1@yahoo.de>
2017-07-17 09:09:35 +0200

    Updated DL Link

--- Windows Afterinstall/DownloadAutomaticWindowsAfterinstall.bat
+++ Windows Afterinstall/DownloadAutomaticWindowsAfterinstall.bat
@@ -14,6 +14,6 @@ echo            Lade neueste Version herunter....
 echo.
 echo.
 REM ********* Das hier ist ein dummy File nur um das neueste release herunterzuladen und zu starten ********
-start https://github.com/FaserF/FaserFQuickTools/releases/download/1.0/AutomaticWindowsAfterinstall.bat
+start https://github.com/FaserF/FaserFQuickTools/releases/download/1.0/AutomaticWindowsAfterinstall.exe
 timeout /T 4
 exit
