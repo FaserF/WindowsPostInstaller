33beebd2d875f9903115a812116cac592b258772
Fabian Seitz <seitzf1@yahoo.de>
2017-12-21 15:46:45 +0100

    V1.0.1.3 - detect execution path

--- Windows Post Installer/WindowsPostInstaller-Business.bat
+++ Windows Post Installer/WindowsPostInstaller-Business.bat
@@ -1,8 +1,8 @@
 @echo off
-set WPIVersion=1.0.1.2
+set WPIVersion=1.0.1.3
 set datum=21.12.2017
 set Description=Automatic Windows Post Installer (Software, Driver, ...) for a fresh Windows Installation.
-set usbpath=D
+set usbpath=%CD%
 title Automatic Windows Post Installer (Business) by Fabian Seitz - V%WPIVersion% - Datum: %datum%
 
 :Default
@@ -17,11 +17,11 @@ if "%internet%" == "Nicht mit dem Internet verbunden" goto :NoInternet
 cd C:\Users\%username%\Downloads\
 rd /s /q C:\Users\%username%\Downloads\CustomInstall\
 md C:\Users\%username%\Downloads\CustomInstall\
-echo                           Windows Post Installer - LOG > %usbpath%:\WPI_Log.txt
-echo             ============================================================ >> %usbpath%:\WPI_Log.txt
-echo %TIME% Programm ist gestartet >> %usbpath%:\WPI_Log.txt
-echo             Probleme? https://github.com/FaserF/FaserFQuickTools/issues >> %usbpath%:\WPI_Log.txt
-echo ######################################################################## >> %usbpath%:\WPI_Log.txt
+echo                           Windows Post Installer - LOG > %usbpath%WPI_Log.txt
+echo             ============================================================ >> %usbpath%WPI_Log.txt
+echo %TIME% Programm ist gestartet >> %usbpath%WPI_Log.txt
+echo             Probleme? https://github.com/FaserF/FaserFQuickTools/issues >> %usbpath%WPI_Log.txt
+echo ######################################################################## >> %usbpath%WPI_Log.txt
 REM *********Erstelle reg Eintrag zur Deaktivierung der Edge Speichern Aufforderung********
 echo Windows Registry Editor Version 5.00 > "C:\Users\%username%\Downloads\CustomInstall\EdgeAutoDownload.reg"
 echo [HKEY_CURRENT_USER\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppContainer\Storage\microsoft.microsoftedge_8wekyb3d8bbwe\MicrosoftEdge\Download] >> "C:\Users\%username%\Downloads\CustomInstall\EdgeAutoDownload.reg"
@@ -57,10 +57,10 @@ for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
 if "%version%" == "6.3" set WindowsVersion=Windows 8
 if "%version%" == "6.1" set WindowsVersion=Windows 7
 if "%version%" == "10.0" set WindowsVersion=Windows 10
-echo %TIME%  %WindowsVersion% wurde automatisch ermittelt >> %usbpath%:\WPI_Log.txt 
-if "%WindowsVersion%" == "Windows 7" echo Treiber Support wird unter Windows 7 nicht Garantiert. >> %usbpath%:\WPI_Log.txt
-if "%WindowsVersion%" == "Windows Version unbekannt" echo Installierte Windows Version konnte nicht erkannt werden. Treiber Support wird nicht Garantiert. >> %usbpath%:\WPI_Log.txt
-echo ######################################################################## >> %usbpath%:\WPI_Log.txt
+echo %TIME%  %WindowsVersion% wurde automatisch ermittelt >> %usbpath%WPI_Log.txt 
+if "%WindowsVersion%" == "Windows 7" echo Treiber Support wird unter Windows 7 nicht Garantiert. >> %usbpath%WPI_Log.txt
+if "%WindowsVersion%" == "Windows Version unbekannt" echo Installierte Windows Version konnte nicht erkannt werden. Treiber Support wird nicht Garantiert. >> %usbpath%WPI_Log.txt
+echo ######################################################################## >> %usbpath%WPI_Log.txt
 cls
 echo             ============================================================
 echo                    Automatische Treiber und Programm Installation
@@ -111,69 +111,69 @@ REM set /p usbpath="Bitte USB Laufwerksbuchstaben angeben (z.B. D): "
 goto :Install
 
 :Install
-echo %SYSMODEL% wurde automatisch ermittelt | echo %TIME% %SYSMODEL% wurde automatisch ermittelt >> %usbpath%:\WPI_Log.txt | echo ######################################################################## >> %usbpath%:\WPI_Log.txt
+echo %SYSMODEL% wurde automatisch ermittelt | echo %TIME% %SYSMODEL% wurde automatisch ermittelt >> %usbpath%WPI_Log.txt | echo ######################################################################## >> %usbpath%WPI_Log.txt
 :Install-Start
-echo Errorlevelerklärungen: 0 (erfolgreich), 1 (fehlgeschlagen), 2 (Neustart erforderlich), 3 (gleiche Version bereits installiert), 5 (dieser Treiber ist fuer ein anderes Geraet), 9059 (Datei wurde nicht gefunden) >> %usbpath%:\WPI_Log.txt | echo ######################################################################## >> %usbpath%:\WPI_Log.txt
+echo Errorlevelerklärungen: 0 (erfolgreich), 1 (fehlgeschlagen), 2 (Neustart erforderlich), 3 (gleiche Version bereits installiert), 5 (dieser Treiber ist fuer ein anderes Geraet), 9059 (Datei wurde nicht gefunden) >> %usbpath%WPI_Log.txt | echo ######################################################################## >> %usbpath%WPI_Log.txt
 
 REM *********Starten der WLAN Installation********
 echo "Starte WLAN Treiber im Silent Mode"
-start /d "%usbpath%:\_Driver\%SYSMODEL%\" Wifi.exe -s -norestart
-echo "Errorlevel (0 erfolgreich, 1 fehlgeschlagen) %errorlevel%" | echo WLAN Errorlevel: %errorlevel% >> %usbpath%:\WPI_Log.txt
+start /d "%usbpath%_Driver\%SYSMODEL%\" Wifi.exe -s -norestart
+echo "Errorlevel (0 erfolgreich, 1 fehlgeschlagen) %errorlevel%" | echo WLAN Errorlevel: %errorlevel% >> %usbpath%WPI_Log.txt
 timeout /T 3 > NUL:
 
 REM *********Starten der Fingerprint Installation********
 echo "Starte Fingerprint Treiber im Silent Mode"
-start /d "%usbpath%:\_Driver\%SYSMODEL%\" Fingerprint.EXE /s
-echo "Errorlevel (0 erfolgreich, 1 fehlgeschlagen) %errorlevel%" | echo Fingerprint Errorlevel: %errorlevel% >> %usbpath%:\WPI_Log.txt
+start /d "%usbpath%_Driver\%SYSMODEL%\" Fingerprint.EXE /s
+echo "Errorlevel (0 erfolgreich, 1 fehlgeschlagen) %errorlevel%" | echo Fingerprint Errorlevel: %errorlevel% >> %usbpath%WPI_Log.txt
 timeout /T 2 > NUL:
 
 REM *********Starten der Touchpad Installation********
 echo "Starte Touchpad Treiber im Silent Mode"
-start /d "%usbpath%:\_Driver\%SYSMODEL%\" Touchpad.EXE /s /f
-echo "Errorlevel (0 erfolgreich, 1 fehlgeschlagen) %errorlevel%" | echo Touchpad Errorlevel: %errorlevel% >> %usbpath%:\WPI_Log.txt
+start /d "%usbpath%_Driver\%SYSMODEL%\" Touchpad.EXE /s /f
+echo "Errorlevel (0 erfolgreich, 1 fehlgeschlagen) %errorlevel%" | echo Touchpad Errorlevel: %errorlevel% >> %usbpath%WPI_Log.txt
 timeout /T 2 > NUL:
 
 REM *********Starten der Grafik Installation********
 echo "Starte Intel Grafik Treiber im Silent Mode"
-start /d "%usbpath%:\_Driver\%SYSMODEL%\" Video.EXE /s
-echo "Errorlevel (0 erfolgreich, 1 fehlgeschlagen) %errorlevel%" | echo Grafik Errorlevel: %errorlevel% >> %usbpath%:\WPI_Log.txt
+start /d "%usbpath%_Driver\%SYSMODEL%\" Video.EXE /s
+echo "Errorlevel (0 erfolgreich, 1 fehlgeschlagen) %errorlevel%" | echo Grafik Errorlevel: %errorlevel% >> %usbpath%WPI_Log.txt
 timeout /T 5 > NUL:
 
 REM *********Starten der Audio Installation********
 echo "Starte Audio Treiber im Silent Mode"
-start /d "%usbpath%:\_Driver\%SYSMODEL%\" Audio.EXE /s
-echo "Errorlevel (0 erfolgreich, 1 fehlgeschlagen) %errorlevel%" | echo Audio Errorlevel: %errorlevel% >> %usbpath%:\WPI_Log.txt
+start /d "%usbpath%_Driver\%SYSMODEL%\" Audio.EXE /s
+echo "Errorlevel (0 erfolgreich, 1 fehlgeschlagen) %errorlevel%" | echo Audio Errorlevel: %errorlevel% >> %usbpath%WPI_Log.txt
 timeout /T 5 > NUL:
 
 REM *********Starten der Mobilfunk Installation********
 echo "Starte Mobilfunk Treiber im Silent Mode"
-start /d "%usbpath%:\_Driver\%SYSMODEL%\" Mobilfunk.EXE /s
-echo "Errorlevel (0 erfolgreich, 1 fehlgeschlagen) %errorlevel%" | echo Mobilfunk Errorlevel: %errorlevel% >> %usbpath%:\WPI_Log.txt
+start /d "%usbpath%_Driver\%SYSMODEL%\" Mobilfunk.EXE /s
+echo "Errorlevel (0 erfolgreich, 1 fehlgeschlagen) %errorlevel%" | echo Mobilfunk Errorlevel: %errorlevel% >> %usbpath%WPI_Log.txt
 timeout /T 3 > NUL:
 
 REM *********Starten der Bluetooth Installation********
 echo "Starte Intel Grafik Treiber im Silent Mode"
-start /d "%usbpath%:\_Driver\%SYSMODEL%\" Bluetooth.exe -s -norestart
-echo "Errorlevel (0 erfolgreich, 1 fehlgeschlagen) %errorlevel%" | echo Bluetooth Errorlevel: %errorlevel% >> %usbpath%:\WPI_Log.txt
+start /d "%usbpath%_Driver\%SYSMODEL%\" Bluetooth.exe -s -norestart
+echo "Errorlevel (0 erfolgreich, 1 fehlgeschlagen) %errorlevel%" | echo Bluetooth Errorlevel: %errorlevel% >> %usbpath%WPI_Log.txt
 timeout /T 5 > NUL:
 
 goto :Exit
 
 :NoInternet
-echo %TIME% Internet Verbindung konnte nicht hergestellt werden! Programm wird offline fortgesetzt. >> %usbpath%:\WPI_Log.txt
+echo %TIME% Internet Verbindung konnte nicht hergestellt werden! Programm wird offline fortgesetzt. >> %usbpath%WPI_Log.txt
 msg * "Keine Internet Verbindung verfuegbar, Programm wird offline fortgesetzt!"
 goto :Start
 
 :Exit
 echo Dialog schließt sich in wenigen Sekunden und loescht lokale Installationsfiles.
 rd /s /q C:\Users\%username%\Downloads\CustomInstall\
-echo ######################################################################## >> %usbpath%:\WPI_Log.txt
-echo %TIME% Installation abgeschlossen. BIOS Update gestartet. >> %usbpath%:\WPI_Log.txt
-echo ######################################################################## >> %usbpath%:\WPI_Log.txt
-start /min %usbpath%:\WPI_Log.txt
+echo ######################################################################## >> %usbpath%WPI_Log.txt
+echo %TIME% Installation abgeschlossen. BIOS Update gestartet. >> %usbpath%WPI_Log.txt
+echo ######################################################################## >> %usbpath%WPI_Log.txt
+start /min %usbpath%WPI_Log.txt
 msg * "Installationen abgeschlossen! Damit die Treiber aktualisiert werden ist ein Neustart erforderlich. BIOS Update wird nun gestartet wenn vorhanden."
 REM *********Starten der BIOS Update Installation********
-start /d "%usbpath%:\_Driver\%SYSMODEL%\" BIOS.exe
+start /d "%usbpath%_Driver\%SYSMODEL%\" BIOS.exe
 echo "Starte BIOS Update"
 slmgr.vbs /ato
 exit
